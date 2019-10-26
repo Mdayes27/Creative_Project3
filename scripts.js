2afb7b947c19307dbb9b145ec3add96dca29cd31
Eli <40049026+Bush2775@users.noreply.github.com>
2019-10-25 19:09:32 -0600

    Adding API call
    See comments

--- scripts.js
+++ scripts.js
@@ -1,9 +1,31 @@
 //enter your vue here
+
 new Vue({
   el: '#root',
   data: {
-    message: 'Welcome to Quotes on Quotes on Quotes'
+    message: 'Find the Wisdom You Seek',
+    name: '',
+    seen: false
   },
-
+  //make the call to the api here
 
 });
+
+
+document.getElementById("getQuote").addEventListener("click", displayQuote) 
+
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+
+function displayQuote() {
+  var xhReq = new XMLHttpRequest();
+  xhReq.open("GET", 'https://api.quotable.io/random', false);
+  xhReq.send(null);
+  var jsonObject = JSON.parse(xhReq.responseText);
+  console.log(jsonObject)
+  document.getElementById("quote").innerHTML = jsonObject.content + " -" + jsonObject.author
+}
+
+
+
+
